b74b60759bf28490d4c6bf61ea89cb101c430b9d
efgoetsch <egoetsch@mail.greenriver.edu>
2019-01-15 01:06:03 -0500

    second commit

---
+++ js-review.js
@@ -0,0 +1,16 @@
+/*  Ellen Goetsch
+    January 14, 2019
+    js-review.js
+    JavaScript file to accompany js-review.html
+*/
+
+//Step one - create a JS program that prints "Introduction to JavaScript" to
+//the console window and test it in your browser
+var x = "JavaScript";
+
+function introConsole(x)
+{
+    console.log("Introduction to", x);
+}
+
+introConsole(x);
